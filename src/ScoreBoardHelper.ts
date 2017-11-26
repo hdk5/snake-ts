4ac2bb7064533430705e71f705a6b9b990757978
hdk5 <humandk55@gmail.com>
2017-11-26 09:46:04 +0300

    v1.0.2: Stricter type checking for ScoreBoard

--- src/ScoreBoardHelper.ts
+++ src/ScoreBoardHelper.ts
@@ -1,61 +1,70 @@
-import Util from "./Util";
+interface ScoreBoard {
+    [key: string]: Array<Score>
+}
+
+interface Score {
+    score: number;
+    date: Date;
+    name: string;
+}
 
 export default class ScoreBoardHelper {
+
     private static LS_KEY = "score_board";
 
-    static getBoard() {
+    static getBoard(): ScoreBoard {
         let sbRaw;
-        let sb = {};
+        let sb: ScoreBoard = {};
         try {
-            let sbs: string = localStorage.getItem(ScoreBoardHelper.LS_KEY);
-            sbRaw = JSON.parse(sbs);
-            if (!Util.isPlainObject(sbRaw)) {
-                sbRaw = {};
-            }
+            let sbS: string = localStorage.getItem(ScoreBoardHelper.LS_KEY);
+            sbRaw = JSON.parse(sbS);
         } catch (ex) {
             sbRaw = {};
         }
         for (let k in sbRaw) {
             if (!sbRaw.hasOwnProperty(k)) continue;
             if (!Array.isArray(sbRaw[k])) continue;
-            (sb as any)[k] = (sbRaw[k] as Array<any>)
+            sb[k] = (sbRaw[k] as Array<any>)
                 .filter(value =>
-                    typeof value.name === "string"
+                    value != null
+                    && value.hasOwnProperty("name")
+                    && value.hasOwnProperty("date")
+                    && value.hasOwnProperty("score")
+                    && typeof value.name === "string"
                     && !isNaN(Date.parse(value.date))
                     && typeof value.score === "number"
-                    && value.score >= 0 && value.score < Infinity)
-                .map(value => {
-                        return {
-                            score: value.score,
-                            date: new Date(Date.parse(value.date)),
-                            name: value.name
-                        }
-                    }
+                    && value.score >= 0)
+                .map(value =>
+                    ({
+                        score: value.score,
+                        date: new Date(value.date),
+                        name: value.name
+                    })
                 );
 
         }
         return sb;
     }
 
-    static saveBoard(board: Object) {
+    static saveBoard(board: ScoreBoard) {
         let sbs: string = JSON.stringify(board);
         localStorage.setItem(ScoreBoardHelper.LS_KEY, sbs);
     }
 
-    static getTopScores(map: string): Array<{ score: number, date: Date, name: string }> {
-        let sb = ScoreBoardHelper.getBoard();
-        let fb: Array<{ score: number, date: Date, name: string }> = sb.hasOwnProperty(map) ? (sb as any)[map] : [];
+    static getTopScores(map: string): Array<Score> {
+        let sb: ScoreBoard = ScoreBoardHelper.getBoard();
+        let fb: Array<Score> = sb.hasOwnProperty(map) ? sb[map] : [];
         return fb
             .filter(value => value.score > 0)
             .sort((a: any, b: any) => b.score - a.score)
             .slice(0, 10);
     }
 
-    static addScore(map: string, score: { score: number, date: Date, name: string }) {
-        let sb = ScoreBoardHelper.getBoard();
-        let fb = sb.hasOwnProperty(map) ? (sb as any)[map] : [];
-        (fb as Array<any>).push(score);
-        (sb as any)[map] = fb;
+    static addScore(map: string, score: Score) {
+        let sb: ScoreBoard = ScoreBoardHelper.getBoard();
+        let fb: Array<Score> = sb.hasOwnProperty(map) ? sb[map] : [];
+        fb.push(score);
+        sb[map] = fb;
         ScoreBoardHelper.saveBoard(sb);
     }
 }
